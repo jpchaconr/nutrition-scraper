c53faba2c1cc4a264b2332f19cba1827932876f1
Juan Pablon Pablo Chacón <jpchacon@uc.cl>
2022-08-01 12:30:49 -0400

    Add jumbo products urls

--- jumbo/navigation.py
+++ jumbo/navigation.py
@@ -7,33 +7,56 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+def get_products_links():
+    timeout_in_seconds = 10
+    WebDriverWait(browser, timeout_in_seconds).until(ec.presence_of_element_located((By.CSS_SELECTOR, 'ul.shelf-list')))
+    html = browser.page_source
+    soup = BeautifulSoup(html, features="html.parser")
+    links = list(map(lambda x: x.find('a', class_='shelf-product-title')['href'], soup.find_all('li', class_='shelf-item')))
+    return list(map(lambda x: 'https://www.jumbo.cl' + x, links))
+
+def write_new_links(file_name, links):
+    with open(file_name, 'a') as file:
+        file.write('\n'.join(links))
+
 sections = [
-    'https://www.jumbo.cl/lacteos', 
-    'https://www.jumbo.cl/despensa', 
-    'https://www.jumbo.cl/frutas-y-verduras', 
-    'https://www.jumbo.cl/carniceria', 
-    'https://www.jumbo.cl/bebidas-aguas-y-jugos'
+    'https://www.jumbo.cl/lacteos',
+    'https://www.jumbo.cl/despensa',
+    'https://www.jumbo.cl/frutas-y-verduras',
+    'https://www.jumbo.cl/carniceria',
+    'https://www.jumbo.cl/bebidas-aguas-y-jugos',
+    'https://www.jumbo.cl/congelados',
+    'https://www.jumbo.cl/desayuno-y-dulces',
+    'https://www.jumbo.cl/comidas-preparadas',
+    'https://www.jumbo.cl/quesos-y-fiambres',
+    'https://www.jumbo.cl/pescaderia',
+    'https://www.jumbo.cl/mundo-bio-natura'
 ]
 
 options = Options()
 options.add_argument('--headless')
 browser = webdriver.Chrome(service=Service('C:/webdriver/chromedriver.exe'), options=options)
 
-browser.get(sections[0])
-timeout_in_seconds = 10
-WebDriverWait(browser, timeout_in_seconds).until(ec.presence_of_element_located((By.CSS_SELECTOR, 'ul.shelf-list')))
-next_page_btn = browser.find_element(By.CLASS_NAME, 'slider-next-button')
-html = browser.page_source
-soup = BeautifulSoup(html, features="html.parser")
+for section in sections[2:]:
+    print(section)
+    print("##############\n"*3)
+    browser.get(section)
+    links = get_products_links()
+    try:
+        write_new_links('products_urls.txt', links)
+    except:
+        print("Something went wrong in page 0")
 
-## next page button
-next_page = soup.find_all('button', class_='slider-next-button')
-span = soup.find('span', class_='select-page-title').text
-pages = span.split(' ')[1::2]
-print(span.split(' ')[1::2])
-print(int(pages[0]) + 1)
+    pages_btns = browser.find_elements(By.CLASS_NAME, 'page-number')
+    next_page_index = 1
+    while next_page_index < len(pages_btns):
+        browser.execute_script("arguments[0].click();", pages_btns[next_page_index])
+        links = get_products_links()
+        try:
+            write_new_links('products_urls.txt', links)
+        except:
+            print("Something went wrong in page", next_page_index)
 
-print(next_page)
-
-links = list(map(lambda x: x.find('a', class_='shelf-product-title')['href'], soup.find_all('li', class_='shelf-item')))
-print(links)
+        print(next_page_index)
+        pages_btns = browser.find_elements(By.CLASS_NAME, 'page-number')
+        next_page_index += 1
